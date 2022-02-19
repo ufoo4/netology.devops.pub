667715137e0199614a2c6baab6ba88af4e5a0d74
Sergey Chipyshev <pochta1943@gmail.com>
2022-02-19 17:52:46 +0500

    added homework 06-db-02-sql.md

---
+++ homeworks/06-db-02-sql/src/terraform/node01.tf
@@ -0,0 +1,31 @@
+resource "yandex_compute_instance" "node01" {
+  name                      = "node01"
+  zone                      = "ru-central1-a"
+  hostname                  = "node01.netology.yc"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 5
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "${var.centos-7-base}"
+      name        = "root-node01"
+      type        = "network-hdd"
+      size        = "10"
+    }
+  }
+
+  network_interface {
+    subnet_id  = "${yandex_vpc_subnet.default.id}"
+    nat        = true
+    ip_address = "192.168.101.11"
+  }
+
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
---
+++ homeworks/06-db-02-sql/src/terraform/variables.tf
@@ -0,0 +1,14 @@
+
+variable "yandex_cloud_id" {
+  default = "b1gffcps5oa5h9clc5o9"
+}
+
+
+variable "yandex_folder_id" {
+  default = "b1g27gpcstr1l1bi3a22"
+}
+
+
+variable "centos-7-base" {
+  default = "fd88bbgjvrb66jdheg9o"
+}
