439500ea2c56981da2604ce89af24560f42b0e84
Sergey Chipyshev <pochta1943@gmail.com>
2022-02-27 10:24:13 +0500

    added homework 06-db-04-postgresql

---
+++ homeworks/06-db-04-postgresql/src/terraform/ansible.tf
@@ -0,0 +1,39 @@
+resource "null_resource" "wait" {
+  provisioner "local-exec" {
+    command = "sleep 100"
+  }
+
+  depends_on = [
+    local_file.inventory
+  ]
+}
+
+resource "null_resource" "docker" {
+  provisioner "local-exec" {
+    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/docker-deploy.yml"
+  }
+
+  depends_on = [
+    null_resource.wait
+  ]
+}
+
+resource "null_resource" "postgres" {
+  provisioner "local-exec" {
+    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/sync-deploy.yml"
+  }
+
+  depends_on = [
+    null_resource.docker
+  ]
+}
+
+resource "null_resource" "start" {
+  provisioner "local-exec" {
+    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/start-docker.yml"
+  }
+
+  depends_on = [
+    null_resource.postgres
+  ]
+}
